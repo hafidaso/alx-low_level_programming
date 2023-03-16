5914bc070d75818eb4dbca158c4f9db54cec760a
Hafida BELAYD <hafidabelaidagnaoui@gmail.com>
2023-03-16 15:29:53 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 
-int main() {
-    printf("The size of char is %ld bytes\n", sizeof(char));
-    printf("The size of short is %ld bytes\n", sizeof(short));
-    printf("The size of int is %ld bytes\n", sizeof(int));
-    printf("The size of long is %ld bytes\n", sizeof(long));
-    printf("The size of long long is %ld bytes\n", sizeof(long long));
-    printf("The size of float is %ld bytes\n", sizeof(float));
-    printf("The size of double is %ld bytes\n", sizeof(double));
-    printf("The size of long double is %ld bytes\n", sizeof(long double));
-    return 0;
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
 }
