579951d289238a780043a89112285ac7f85d6b53
Hafida BELAYD <hafidabelaidagnaoui@gmail.com>
2023-03-18 05:27:25 -0700

    Modification for Positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,7 +15,7 @@ int main(void)
 	{
 	    printf("%d is %s\n", n, "negative");
 	}
-	else if(n > 0)
+	else if (n > 0)
 	{
 	    printf("%d is %s\n", n, "positive");
 	}
@@ -23,6 +23,6 @@ int main(void)
 	{
 	    printf("%d is %s\n", n, "zero");
 	}
-	
 	return (0);
+	
 }
