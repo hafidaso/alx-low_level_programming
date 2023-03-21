d987f8ed2a8926d8a0b09418751296516f1c354b
Hafida BELAYD <hafidabelaidagnaoui@gmail.com>
2023-03-21 07:36:04 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,15 +6,18 @@
   *
   * Return: 1 for positive num, -1 for negative num or zero for anything else
   */
-int print_sign(int n) {
-    if (n > 0) {
-        printf("+");
+int print_sign(int n)
+{
+    if (n > 0)
+    {
+        _putchar("+");
         return 1;
-    } else if (n == 0) {
-        printf("0");
+    } else if (n == 0)
+    {
+        _putchar("0");
         return 0;
     } else {
-        printf("-");
+        _putchar("-");
         return -1;
     }
 }
