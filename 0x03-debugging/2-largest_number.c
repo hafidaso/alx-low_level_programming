c6289c60c008341e36209d861e2e6b96607eadab
Hafida BELAYD <hafidabelaidagnaoui@gmail.com>
2023-03-22 09:05:44 -0700

    main.h

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c, max;
+    
+    printf("Enter three integers: ");
+    scanf("%d %d %d", &a, &b, &c);
+    
+    // find the maximum value among the three integers
+    max = a;
+    if (b > max) {
+        max = b;
+    }
+    if (c > max) {
+        max = c;
+    }
+    
+    printf("The largest integer is %d\n", max);
+    
+    return 0;
+}
