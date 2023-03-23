a60f69709d6632e777dbdaba2fbdcd54081b5159
Hafida BELAYD <hafidabelaidagnaoui@gmail.com>
2023-03-23 16:45:59 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,15 +8,18 @@
 
 void print_line(int n)
 {
-    int i;
-    if (n <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
-    for (int i = 0; i < n; i++)
-    {
-        _putchar('_');
-    }
-    _putchar('\n');
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
 }
