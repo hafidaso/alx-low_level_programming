2b8dbb06f017072d41ff60d2737dec95d6767483
Hafida BELAYD <hafidabelaidagnaoui@gmail.com>
2023-03-23 17:04:01 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,21 +6,19 @@
  * Return: Always 0.
  */
 
-
 int main(void)
+
 {
-    long int n = 612852475143;
-    long int i;
+	long n, i;
 
-    for (i = 2; i <= n; i++)
-    {
-        while (n % i == 0)
-        {
-            n /= i;
-        }
-    }
+	n = 612852475143;
+	for (i = 2; i < n; i++)
+	{
+		while (n % i == 0)
+			n = n / i;
+	}
 
-    printf("%ld\n", i - 1);
+	printf("%lu\n", n);
 
-    return 0;
+	return (0);
 }
