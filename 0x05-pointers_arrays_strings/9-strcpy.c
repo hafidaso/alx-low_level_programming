47cc41e8b69edd80f577aa07f73f766792553ffa
Hafida BELAYD <hafidabelaidagnaoui@gmail.com>
2023-03-28 15:06:42 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,17 +8,19 @@
   *
   * Return: the pointer to dest
   */
-char *_strcpy(char *dest, char *src) {
-    char *dest_start = dest;
+char *_strcpy(char *dest, char *src)
+{
+	int l = 0;
+	int x = 0;
 
-    while (*src != '\0') {
-        *dest = *src;
-        dest++;
-        src++;
-    }
-
-    *dest = '\0';
-
-    return dest_start;
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
+	for ( ; x < l ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[l] = '\0';
+	return (dest);
 }
-
