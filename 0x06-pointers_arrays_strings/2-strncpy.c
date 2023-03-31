6ecf090fa5792c334c5dfde2f4ebf46865f06b5e
Hafida BELAYD <hafidabelaidagnaoui@gmail.com>
2023-03-30 18:12:09 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,18 +1,32 @@
 #include "main.h"
+
 /**
- *_strncpy - Function that copies a string
- *@dest: Destination
- *@src: source
- *@n: Size in bytes
- *Return: always 0-success
+ * _strncpy - a function that print n elem of array
+ * @dest: pointer variable
+ * @src: variable
+ * @n: variable
+ * Return: void return nothing
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		src[i] = dest[i];
-	for (; i < n; i++)
-		dest[i] = '\0';
+	int i = 0;
+	int j = 0;
+
+	while (src[j])
+		j++;
+	while (src[i] && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (j < n)
+	{
+		while (i < n)
+		{
+			dest[i] = '\0';
+			i++;
+		}
+	}
 	return (dest);
 }
