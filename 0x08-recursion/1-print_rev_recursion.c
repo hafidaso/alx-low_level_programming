100c51c3e2fd5f9b41156d48693bcd06c936af37
Hafida BELAYD <hafidabelaidagnaoui@gmail.com>
2023-04-05 17:58:38 -0700

    rev

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ *_print_rev_recursion - prints a string in reverse
+ *@s: an array of pointers
+ *Return: nothing void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		    return;
+		    
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
