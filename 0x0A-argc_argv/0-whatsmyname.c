7e984fdff47ea88b9e8741101eba2fb358046108
Hafida BELAYD <hafidabelaidagnaoui@gmail.com>
2023-04-08 21:02:42 -0700

    first taask

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program, followed by a new line.
+ * @argc: int
+ * @argv : list
+ * Return: Always 0.
+ */
+
+int main(int c, char **v)
+{
+	if (c > 0)
+		printf("%s\n", *v);
+	return (0);
+}
