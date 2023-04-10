4f3bedfa5f971788a84d7c7d90cfe763ca04e577
Hafida BELAYD <hafidabelaidagnaoui@gmail.com>
2023-04-10 16:36:54 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,21 +2,21 @@
 #include <unistd.h>
 
 /**
- * main - prints all command-line arguments, one per line
- * @ar: the Var
- * @argv: The Var
- *
+ * main - The Function
+ * @x:  The variable
+ * @v: THe variable
  * Return: The Return value/void
  */
-int main(int ar, char *argv[])
+
+int main(int x, char **v)
 {
-        int i;
-
-        /* loop through all arguments and print them */
-        for (i = 0; i < ar; i++)
-        {
-                printf("%s\n", argv[i]);
-        }
-
-        return (0);
+	if (x >= 1)
+	{
+		while (*v)
+		{
+			printf("%s\n", *v);
+			v++;
+		}
+	}
+	return (0);
 }
