64b483402d4cceee217121ee9c362035cf31f73e
Hafida BELAYD <hafidabelaidagnaoui@gmail.com>
2023-04-10 16:15:09 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main -  program that prints the number of arguments passed into it
+ * @argc: the number of command line arguments
+ * @argv: an array of command line argument strings
+ *
+ * Return: The Return value/void
+ */
+ 
+ int main(int argc, char *argv[])
+{
+        printf("%d\n", argc - 1);
+        return (0);
+}
