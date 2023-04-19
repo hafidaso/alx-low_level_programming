a73adffbc7d2f59604a7b87ab5cb55934bb4de23
Hafida BELAYD <hafidabelaidagnaoui@gmail.com>
2023-04-18 18:27:59 -0700

    5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -12,9 +12,11 @@ void free_dog(dog_t *d)
 
 	if (d)
 	{
-		free(d->name);
-        free(d->owner);
-        free(d);
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
 	}
 
 }
