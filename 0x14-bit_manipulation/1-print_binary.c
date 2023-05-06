d7c167aeceb7128a85b7de8503bba59631158cc7
hafidaso <hafidabelaidagnaoui@gmail.com>
2023-05-06 04:14:35 -0700

    2 task

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* print_binary - prints the binary representation of a number
+* @n: number to print
+* Return: void
+*/
+void print_binary(unsigned long int n)
+{
+	int cmp, c = 0;
+	unsigned long int current;
+
+	for (cmp = 63; cmp >= 0; cmp--)
+	{
+		current = n >> cmp;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			c++;
+		}
+		else if (c)
+			_putchar('0');
+	}
+	if (!c)
+		_putchar('0');
+}
