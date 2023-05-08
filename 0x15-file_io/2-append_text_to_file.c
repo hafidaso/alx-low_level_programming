5fa726768fd17f10fda57a415f3ddaa5326cd69f
hafidaso <hafidabelaidagnaoui@gmail.com>
2023-05-08 16:10:04 -0700

    Tadk

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,35 +2,36 @@
 
 /**
  *append_text_to_file - appends text at the end of file
- *@filename: the file in hand
+ *@filename: the name of the file
  *@text_content: the text to add at the end of the file
  *Return: 1 on success else -1 is returned
  */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
-	int w;
-	int slen;
+    int fd, res;
+    ssize_t length = 0;
 
-	slen = 0;
-	w = 0;
-	if (!filename)
-		return (-1);
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd < 0)
-		return (-1);
-	if (text_content)
-	{
-		while (text_content[slen])
-			slen++;
-		w = write(fd, text_content, slen);
-		if (w < 0 || slen != w)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
-	close(fd);
-	return (1);
+    if (filename == NULL)
+        return (-1);
+
+    fd = open(filename, O_WRONLY | O_APPEND);
+    if (fd == -1)
+        return (-1);
+
+    if (text_content != NULL)
+    {
+        while (text_content[length])
+            length++;
+
+        res = write(fd, text_content, length);
+        if (res == -1)
+        {
+            close(fd);
+            return (-1);
+        }
+    }
+
+    close(fd);
+    return (1);
 }
