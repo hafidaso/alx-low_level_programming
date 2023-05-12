bb522c8140a45ec072d767a903cf491bf62dc93b
hafidaso <hafidabelaidagnaoui@gmail.com>
2023-05-11 17:28:14 -0700

    Modification Main

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,8 +5,10 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
-void _putchar(int c);
+int create_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
 #endif
